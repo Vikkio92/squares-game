4bbf527dfd0dc84bb51bd4c32809d0a720fd49a5
Vikkio92 <vittorio.santagostino@gmail.com>
2023-06-08 22:50:23 +0100

    8 June EOD commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,8 @@ var fireAgain = 0;
 var level = 1;
 var lives = 0;
 var points = 0;
+var squareSpeed = 4;
+
 
 // Initialise button to start the game
 const startButton = document.getElementById('button-start');
@@ -22,19 +24,46 @@ startButton.addEventListener('click', () => {
 function startGame() {
   isGameOn = 1;
   level = 1;
-  lives = 5;
-  const livesParagraph = document.createElement('p');
-  livesParagraph.innerHTML = 'Lives: ';
-  document.querySelector('.lives').appendChild(livesParagraph);
-  for (let i = 0; i < lives; i++) {
+  lives = 2;
+  const livesContainer = document.querySelector('.lives');
+  const scoreContainer = document.querySelector('.score');
+  const levelContainer = document.querySelector('.level');
+  // Lives
+  livesContainer.innerHTML = 'Lives: ';
+  //livesContainer.appendChild(livesParagraph); // append new paragraph to container
+  for (let i = 0; i < lives; i++) { // append X number of life squares
     const life = document.createElement('div');
     life.className = 'life';
-    document.querySelector('.lives').appendChild(life);
+    livesContainer.appendChild(life);
   };
+  livesContainer.style.cssText  = 'display: flex; justify-content: center; font-size: 1vw; z-index: inherit;';
+  // Score
+  scoreContainer.innerHTML = 'Score: ' + points;
+  scoreContainer.style.cssText  = 'display: flex; justify-content: center; font-size: 1vw; z-index: inherit;';
+  // Level
+  levelContainer.innerHTML = 'Level: ' + level;
+  levelContainer.style.cssText  = 'display: flex; justify-content: center; font-size: 1vw; z-index: inherit;';
+
   fireSquare();
 };
 
 
+// TO DO
+// End the game when lives reach zero
+function endGame() {
+  document.querySelector('#instructions').innerHTML = 'Game over! Play again?';
+  document.querySelector('.lives').style.display = 'none';
+  document.querySelector('.score').style.display = 'none';
+  document.querySelector('.level').style.display = 'none';
+  const startButton = document.getElementById('button-start');
+  startButton.style.display  = 'block';
+  startButton.style.position = 'absolute';
+  startButton.style.left = '50%';
+  startButton.style.transform = 'translateX(-50%)';
+}
+
+
+// When the user clicks the square while it's within bounds, score a point, remove the square and fire off another
 function scorePoint() {
   var square = document.querySelector('#square');
   var squareLeft = parseInt(square.style.left);
@@ -46,10 +75,32 @@ function scorePoint() {
     squareTop <= gridBottomBorder
     ) {
       points += 1;
+      document.querySelector('.score').innerHTML = 'Score: ' + points;
       fireAgain = 1;
-      console.log(points);
     }
+  updateLevel();
 };
+
+
+// Update level based on points
+function updateLevel() {
+  if (points % 5 === 0) {
+    level += 1;
+    document.querySelector('.level').innerHTML = 'Level: ' + level;
+  }
+}
+
+
+// Lose a life if you miss a square and check if game over
+function loseLife() {
+  lives -= 1;
+  document.querySelector('.lives').removeChild(document.querySelector('.lives').lastChild);
+  if (lives === 0) {
+    isGameOn = 0;
+    endGame();
+  }
+}
+
 
 // Calculate the left and right boundaries of the page
 var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
@@ -72,7 +123,7 @@ var gridRightBorder = gridTopLeftX + viewportWidth;
 var gridBottomBorder = gridTopLeftY + viewportHeight;
 
 
-// Function that initialises the square and its initial position coordinates, direction, and speed, and displays it
+// Function that initialises the square and its initial position coordinates and direction, and displays it
 function fireSquare() {
   fireAgain = 0;
   // Create a colored square element
@@ -111,21 +162,28 @@ function fireSquare() {
   var directionX;
   var directionY;
 
-  // If the square starts in the left hand side of the screen, direction right, else direction left
-  startPosX <= (0.5 * viewportWidth) ? directionX = Math.random() : directionX = -Math.random();
+  // Set target at the center of the screen
+  var targetX = document.documentElement.clientWidth * 0.5;
+  var targetY = document.documentElement.clientHeight * 0.5;
 
-  // If the square starts in the top side of the screen, direction down, else direction up
-  startPosY <= (0.5 * viewportHeight) ? directionY = Math.random() : directionY = -Math.random();
+  // Calculate the X and Y distance between the starting point of the square and the center of the screen
+  var deltaX = targetX - startPosX;
+  var deltaY = targetY - startPosY;
+
+  // Calculate the angle between the point where the square has spawned and the center of the screen
+  var angle = Math.atan2(deltaY, deltaX);
+
+  // Calculate the direction
+  var directionX = Math.cos(angle) + Math.random() * 0.3 - 0.2; // add a random value between -0.2 and 0.2
+  var directionY = Math.sin(angle) + Math.random() * 0.3 - 0.2;
 
   // Normalize the direction vector
   var magnitude = Math.sqrt(directionX * directionX + directionY * directionY);
   directionX /= magnitude;
   directionY /= magnitude;
 
-  // Initialise square's speed variable
-  var squareSpeed = 2;
-
-  var animationParameters = {directionX:directionX, directionY:directionY, speed:squareSpeed};
+  // direction parameters for animation
+  var animationParameters = {directionX:directionX, directionY:directionY};
 
   animateSquare(animationParameters);
 };
@@ -137,25 +195,29 @@ function animateSquare(animationParameters) {
   var currentPosY = parseFloat(square.style.top); // Get the current top position of the square element as a floating-point number
 
   // Update the square's position
-  var newPosX = currentPosX + animationParameters.directionX * animationParameters.speed; // Calculate the new left position by adding the direction multiplied by squareSpeed to the current left position
-  var newPosY = currentPosY + animationParameters.directionY * animationParameters.speed; // Calculate the new top position by adding the direction multiplied by squareSpeed to the current top position
+  var newPosX = currentPosX + animationParameters.directionX * squareSpeed; // Calculate the new left position by adding the direction multiplied by squareSpeed to the current left position
+  var newPosY = currentPosY + animationParameters.directionY * squareSpeed; // Calculate the new top position by adding the direction multiplied by squareSpeed to the current top position
   square.style.left = newPosX + 'px'; // Set the left position of the square element to the new left position
   square.style.top = newPosY + 'px'; // Set the top position of the square element to the new top position
 
   // Check if the square is still within the viewport
   if (
-    newPosX < 0 ||
+    square &&
+    (newPosX < 0 ||
     newPosX + square.offsetWidth > viewportWidth || // Check if the right edge of the square is outside the viewport width
     newPosY < 0 ||
-    newPosY + square.offsetHeight > viewportHeight // Check if the bottom edge of the square is outside the viewport height
-    || fireAgain === 1
+    newPosY + square.offsetHeight > viewportHeight) // Check if the bottom edge of the square is outside the viewport height
   ) {
-    // If the square is out of bounds, remove it from the page
+    // If the square is out of bounds, remove it from the page and lose a life
     document.body.removeChild(square);
+    loseLife();
     // after removing the out of bounds square, check if the game is still going. If so, fire off a new square
     if (isGameOn === 1) {
       fireSquare();
     }
+  } else if (fireAgain === 1) {
+    document.body.removeChild(square);
+    fireSquare();
   } else {
     // Otherwise, continue animating
     requestAnimationFrame(() => animateSquare(animationParameters));
